decd0ccc1f66b10c98d69a7218d8e1a0bc3fa3fa
Ana Santos <17-10602@usb.ve>
2024-08-04 20:38:14 -0400

    MODIFIED: models files

--- Grupo06/database/models/tag.model.js
+++ Grupo06/database/models/tag.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, Sequelize, DataTypes) => {
             allowNull: false
         }
     }, {
-        timestamps: true,
+        timestamps: false,
         underscored: true,
     });
     return Tag;
